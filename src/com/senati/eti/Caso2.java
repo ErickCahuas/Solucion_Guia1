833c3f47c7389ebaa845262234ca096ca309e61a
51921 <51921@LAPTOP-NNHIOOM7>
2021-04-23 12:03:08 -0500

    Proyecto Parcial - Guia1 (22/04)

---
+++ src/com/senati/eti/Caso2.java
@@ -0,0 +1,26 @@
+package com.senati.eti;
+
+// Importando la librería para lectura de datos
+import java.util.Scanner;
+
+public class Caso2 {
+
+	public static void main(String[] args) {
+		// Instancia a la clase Scanner
+		// Creado un objeto sc a partir de la clade Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Declarar la variable y asignar un valor
+		int numero = 0;
+		
+		// Lectura
+		System.out.print("Ingrese número: ");
+		numero = sc.nextInt();
+		
+		// Impresión
+		System.out.println("Número Ingresado");
+		System.out.println("----------------");
+		System.out.println("Número....: " + numero);
+	}
+}
+
