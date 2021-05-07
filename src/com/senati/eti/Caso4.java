f9ccd83b42ecb51f7132075f358e8a965aa2a3c9
51921 <51921@LAPTOP-NNHIOOM7>
2021-05-07 01:34:33 -0500

    Proyecto Parcial - Guia1 (07/05)

--- src/com/senati/eti/Caso4.java
+++ src/com/senati/eti/Caso4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Caso4 {
 
 	public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		float num1 = 0, num2 = 0;
 		
