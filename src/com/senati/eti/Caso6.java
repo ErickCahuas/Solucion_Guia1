5c0ac841c4c10d997f52a4c9ded5976ec820269a
51921 <51921@LAPTOP-NNHIOOM7>
2021-05-07 01:31:24 -0500

    Caso6

--- src/com/senati/eti/Caso6.java
+++ src/com/senati/eti/Caso6.java
@@ -6,13 +6,17 @@ public class Caso6 {
 		// CAST -> Convertir de un tipo de dato a otro
 		int rpta1 = (int) Math.pow(5, 3);
 		// Raiz cuadrada -> base elevada a la inversa del exp.
-		float rpta2 = (float)  Math.pow(81, 1/2.0);
-		
+		float rpta2 = (float)  Math.pow(81, 1 /  2.0);
+		float rpta3 = (float)  Math.pow(125, 1 / 3.0);
+		float rpta4 = (float)  Math.pow(256, 1 / 4.0);
+		float rpta5 = (float)  Math.pow(25, 1 / 2.0);
+				
 		System.out.println("\nResultados");
 		System.out.println("------------");
 		System.out.println("Respuesta 1...: " + rpta1);
 		System.out.println("Respuesta 2...: " + rpta2);
+		System.out.println("Respuesta 3...: " + rpta3);
+		System.out.println("Respuesta 4...: " + (rpta4 + rpta5));
 
 	}
-
 }
